36ece9f0ad439a613fe680e747b0f29434c40d1c
Palash Kulshrestha <trickypalash@gmail.com>
2023-05-14 13:13:00 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "bus-tracker-40677",
-  url: "https://bus-tracker-40677.botics.co",
-  api: "https://bus-tracker-40677.botics.co/api/v1"
-}
+  "name": "bus-tracker-40677",
+  "url": "https://bus-tracker-40677.botics.co",
+  "api": "https://bus-tracker-40677.botics.co/api/v1",
+  "initialRoute": "Maps"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
