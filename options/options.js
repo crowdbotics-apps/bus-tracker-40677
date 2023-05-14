ce5bd731520f04cc5ee61ccb3e80549c32388a4a
Crowdbotics <team@crowdbotics.com>
2023-05-14 07:22:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bus-tracker-40677",
+  url: "https://bus-tracker-40677.botics.co",
+  api: "https://bus-tracker-40677.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
